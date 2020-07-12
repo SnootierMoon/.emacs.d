211e0707b6b7c4d2a8bf50f29021b7bb7fe3e6a8
Akshay Trivedi <aku24.7x3@gmail.com>
2020-07-11 21:41:30 -0700

    First Commit

---
+++ init.el
@@ -0,0 +1,15 @@
+;;; init.el --- Initialize emacs
+
+;;; Commentary:
+
+;; This file only loads .emacs.d/etc/config.org with org-babel.
+;; Packages and configurations are in .emacs.d/etc/config.org.
+;; The .emacs.d/etc directory is also `no-littering-etc-directory'.
+
+;;; Code:
+
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "etc/config.org")))
+
+(provide 'init)
+
+;;; init.el ends here
